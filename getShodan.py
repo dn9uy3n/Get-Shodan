d7a92332e136751c45d7ab07d627a322b5044f90
dn9uy3n <hoangdiepnguyen96@gmail.com>
2020-03-23 10:02:59 +0700

    Add files via upload
    Add the limit parameter

--- getShodan.py
+++ getShodan.py
@@ -1,5 +1,6 @@
 import argparse
 import shodan
+import time
 
 def filter(result, filter_str, f):
     filter_list = filter_str.split('/')
@@ -12,41 +13,72 @@ def filter(result, filter_str, f):
         f.write(tmp_str + '\n')
 
 def main():
-    parser = argparse.ArgumentParser(description= "get data from Shodan")
-    parser.add_argument("-q", "--query", type = str, metavar = "<\'string query\'>", help = "String query search on Shodan")
-    parser.add_argument("-k", "--key", type = str, metavar = "<shodan_API_key>", help = "API key in Shodan")
-    parser.add_argument("-o", "--output", type = str, metavar= "<output file>", help = "Output file")
-    parser.add_argument("-f", "--filter", type = str, metavar= "ip_str/port/os/host/", help = "filer string")
+    print   """  ____      _     ____  _               _             
+ / ___| ___| |_  / ___|| |__   ___   __| | __ _ _ __  
+| |  _ / _ \ __| \___ \| '_ \ / _ \ / _` |/ _` | '_ \ 
+| |_| |  __/ |_   ___) | | | | (_) | (_| | (_| | | | |
+ \____|\___|\__| |____/|_| |_|\___/ \__,_|\__,_|_| |_|\n"""
+
+    try:
+        parser = argparse.ArgumentParser(description= "get data from Shodan")
+        parser.add_argument("-q", "--query", type = str, metavar = "<\'string query\'>", help = "String query search on Shodan", required=True)
+        parser.add_argument("-k", "--key", type = str, metavar = "<shodan_API_key>", help = "API key in Shodan", required=True)
+        parser.add_argument("-o", "--output", type = str, metavar = "<output file>", help = "Output file", required=True)
+        parser.add_argument("-f", "--filter", type = str, metavar = "ip_str/port/os/host/", help = "Filer string", required=True)
+        parser.add_argument("-os", "--offset", type = int, metavar = "<offet_number>", help = "The serial number of result will begin downloading")
+        parser.add_argument("-l", "--limit", type = int, metavar = "<limit_number>", help = "The number of result will download")
+    except Exception:
+        parser.print_help()
 
     args = parser.parse_args()
     query = args.query
     key = args.key
     filter_str = args.filter
     file_str = args.output
+    offset = args.offset
+    limit_ = args.limit
 
     api = shodan.Shodan(key)
-
     result_number = api.count(query)
-
-    print file_str
+    total = result_number['total']
 
     f = open(file_str, "w")
 
-    print ("Search query: %s has %s result" %(query, result_number['total']))
+    print ("Search query: %s has %s result" %(query, total))
 
-    cnt = 0
+    if offset > 0:
+        cnt = offset
+    else:
+        cnt = 0
+
+    if limit_ > 0:
+        total = cnt + limit_
+
+    check = 0
 
     while True:
-        tmp_limit = int(result_number['total']) - cnt
-        if( tmp_limit < 10000):
-            result = api.search(query, offset = cnt, limit = tmp_limit)
+        try:
+            tmp_limit = int(total) - cnt
+            if( tmp_limit < 1000):
+                result = api.search(query, offset = cnt, limit = tmp_limit)
+                check = 0
+                filter(result, filter_str, f)
+                print "download %d/%d" %(cnt + tmp_limit,total)
+                break
+            result = api.search(query, limit=1000, offset=cnt)
+            check = 0
             filter(result, filter_str, f)
-            print "hanle %d/%d" %(cnt + tmp_limit,result_number['total'])
-            break
-        result = api.search(query, limit=10000, offset=cnt)
-        filter(result, filter_str, f)
-        cnt += 10000
-        print "hanle %d/%d" %(cnt,result_number['total'])
+            cnt += 1000
+            print "download %d/%d" %(cnt,total)
+        except shodan.exception.APIError as e:
+            check += 1
+            if(check > 10):
+                print "Exit program after 10 time reconnection, because"
+                print e
+                break
+            print e
+            print "The program will continue for 10 seconds..."
+            time.sleep(10)
     
     f.close()
 
